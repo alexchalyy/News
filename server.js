c721f517de4f2c279a01c826e232d2ee5ecb2a81
alex chalyy <alexchalyy@gmail.com>
2019-06-01 17:13:08 -0400

    db name change

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/heroku_71qld7r3";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
